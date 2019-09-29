14e230be130324cbe9e8fab7ef5b7de273733c01
jacobcolbert <jacob.colbert@gmail.com>
2019-09-29 18:26:19 -0400

    Add API endpoint

--- Application/Application/.ipynb_checkpoints/views-checkpoint.py
+++ Application/Application/.ipynb_checkpoints/views-checkpoint.py
@@ -3,15 +3,19 @@ from numpy import linalg as LA
 import pickle
 import itertools
 import pathlib
-
+from spacy.lang.en.stop_words import STOP_WORDS
 
 from flask import render_template
 from Application import app as flaskapp
 from flask import request as flaskrequest
+from flask import jsonify
 
 
 nlp = spacy.load('/home/jcolbert/anaconda3/envs/lexical/lib/python3.6/site-packages/en_vectors_web_lg/en_vectors_web_lg-2.0.0')
-
+for word in STOP_WORDS:
+    for w in (word, word[0].capitalize(), word.upper()):
+        lex = nlp.vocab[w]
+        lex.is_stop = True
 
 test_words = pickle.load(open('../PSUTherapy/words.p','rb'))
 flaskapp.logger.info(len(test_words))
@@ -31,11 +35,11 @@ def index():
   query = ''
   if flaskrequest.method == 'POST':
     form_data=dict(flaskrequest.form)
-    print(form_data['word1'])
-    query = form_data['word1']
-    scored_words = get_scored_words(nlp(form_data['word1'][0]))[:10]
+    query = nlp(form_data['word1'][0].lower())
+    query=nlp(''.join([token.string for token in query if not token.is_stop]))
+    scored_words = get_scored_words(query)[:10]
     scored_words = construct_table(scored_words)
-    flaskapp.logger.info(form_data['word1'][0][:10])
+    flaskapp.logger.info(query)
   return render_template("index.html", title = 'Home', scored_words = scored_words, query = query)
 
 def construct_table(scored_words):
@@ -43,3 +47,18 @@ def construct_table(scored_words):
     for scored_word in scored_words:
         res+='<tr><td>{word}</td><td class="text-right">{score:0.2f}</td></tr>'.format(word=scored_word[0],score=scored_word[1])
     return res
+
+@flaskapp.route('/api',methods=['GET'])  
+def api():
+    scored_words = ''
+    query = ''
+    query = nlp(flaskrequest.args.get("query").lower())
+    query=nlp(''.join([token.string for token in query if not token.is_stop]))
+    scored_words = get_scored_words_dict(query)[:10]
+    flaskapp.logger.info(query)
+    return jsonify(scored_words)
+
+def get_scored_words_dict(challenge):
+    scores_map={token.text:challenge.similarity(token) for token in test_tokens}
+    sorted_words = sorted(scores_map.items(), key=lambda kv: kv[1],reverse=True)
+    return [{"word":scored_word[0],"score":scored_word[1]} for scored_word in sorted_words]
